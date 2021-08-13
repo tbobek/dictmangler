6f2d19e6cc190550d0981cada971421706d75b06
T8405580 <thomas.bobek@saint-gobain.com>
2021-08-13 09:52:52 +0200

    tests with arrays successful

--- dictchange.py
+++ dictchange.py
@@ -12,7 +12,7 @@ class DictMangler:
     def set_dict(self, d): 
         self.data = d
 
-    def nested_change(self, k : str): 
+    def nested_change(self, k : str) ->dict: 
         """ removes a key k and replaces its value to be the value of the key of the upper level
         e.g. if input data is { "a" : { "date" : { "$date" : "2021-08-12" }}} 
         and key is "$date" the output will be 
@@ -56,6 +56,59 @@ class DictMangler:
         if len(self.path)>0: 
             self.path.pop()
 
+    @classmethod
+    def dict2list(self, dict_obj : dict, delim : str = "; ") -> list: 
+        """transforms a dictionary into a csv like format 
+        with each line representing a 
+
+        Args:
+            dict_obj (dict): [description]
+            delim (str): [description]
+
+        Returns:
+            list: [description]
+        """
+        arr = []
+        for pair in self.__nested_dict_pair_iterator(dict_obj):
+            if len(pair)>0:
+                value = pair[-1]
+                key = '_'.join(pair[:-1])
+                arr.append(delim.join([key, str(value)]))
+        return arr 
+
+
+    @classmethod
+    def __nested_dict_pair_iterator(self, dict_obj : dict) -> list:
+        """This function accepts a nested dictionary as argument
+            and iterate over all values of nested dictionaries
+
+        Args:
+            dict_obj (dict): dictionary to be transformed
+
+        Returns:
+            list: nothing
+
+        Yields:
+            Iterator[list]: lists representing individual key value pairs 
+            in the dictionary
+        """
+        # Iterate over all key-value pairs of dict argument
+        for key, value in dict_obj.items():
+            # Check if value is of dict type
+            if isinstance(value, dict):
+                # If value is dict then iterate over all its values
+                for pair in  self.__nested_dict_pair_iterator(value):
+                    yield (key, *pair)
+            elif isinstance(value, list): 
+                # if value is list then iterate over all its elements
+                for i,el in enumerate(value):
+                    for pair in self.__nested_dict_pair_iterator(el): 
+                        yield('_'.join([key, str(i)]), *pair)
+            else:
+                # If value is not dict type then yield the value
+                yield (key, value)
+
+        
  
 
 mydict = {
--- test_dictchange.py
+++ test_dictchange.py
@@ -1,8 +1,11 @@
 import unittest
+import json
 from dictchange import DictMangler
 
-class TestDictChange(unittest.TestCase): 
-    def Test1(self): 
+class TestMain(unittest.TestCase): 
+    def test_nested_change(self): 
+        """basic test of reducing one key value pair to its value 
+        """
         mydict = {
             "a" : 2,
             "b" : "test", 
@@ -18,15 +21,72 @@ class TestDictChange(unittest.TestCase):
         c.set_dict(mydict)
         result = c.nested_change("$date")
         expected = "2021-08-12"
-        actual = result["g"]["date"]
+        actual = result["c"]["date"]
+
         self.assertEqual(actual, expected, msg="1. got {}, expected {}".format(actual, expected))
         expected = "2021-08-10"
         actual = result["g"][0]["date"]
         self.assertEqual(actual, expected, msg="2. got {}, expected {}".format(actual, expected))
-        expected = "2021-08-19"
+        expected = "2021-08-09"
         actual = result["h"]["i"]["date"]
         self.assertEqual(actual, expected, msg="3. got {}, expected {}".format(actual, expected))
         
+    def test_dict2list1(self):
+        """handling of array should produce proper output
+        """
+        mydict = {
+            "a" : 2,
+            "b" : "test", 
+            "c" : {
+                "date" : { "$date" : "2021-08-12" }, 
+                "e": "current"
+            },
+            "f" : "some_value",
+            "g" : [ { "date" : { "$date" : "2021-08-10"}}, {"foo" : "baz"}, {"bla": "blubb"}], 
+            "h" : { "i" : { "date" : { "$date" : "2021-08-09" }}}
+        }
+        result = DictMangler.dict2list(mydict)
+        actual = result[0]
+        expected = "a; 2"
+        self.assertEqual(actual, expected, msg="got {}, expected {}".format(actual, expected))
+
         
+    def test_dict2list2(self):
+        """input document now has array with dicts, two entries in this array have 
+        equal keys ("foo"). By enumeration with the array indices, the final output keys
+        should be unique (tested with key "g_1_foo")
+        """
+        mydict = {
+            "a" : 2,
+            "b" : "test", 
+            "c" : {
+                "date" : { "$date" : "2021-08-12" }, 
+                "e": "current"
+            },
+            "f" : "some_value",
+            "g" : [ { "date" : { "$date" : "2021-08-10"}}, {"foo" : "baz"}, {"foo": "blubb"}], 
+            "h" : { "i" : { "date" : { "$date" : "2021-08-09" }}}
+        }
+        result = DictMangler.dict2list(mydict)
+        actual = result[6]
+        expected = "g_1_foo; baz"
+        self.assertEqual(actual, expected, msg="got {}, expected {}".format(actual, expected))
+
+    def test_load1(self):
+        """a more real world scenario with loading data from a json file
+        """
+        mydict = {}
+        with open("test.json") as f: 
+            input_data = json.load(f)
+        if isinstance(input_data, list): 
+            mydict["data"] = input_data
+        else: 
+            mydict = input_data
+        result = DictMangler.dict2list(mydict)
+        actual = result[0]
+        expected = "data_0_type; -"
+        self.assertEqual(actual, expected, msg="got {}, expected {}".format(actual, expected))
+
+
 if __name__ == "__main__": 
     unittest.main()
